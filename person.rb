a9576b9d8ad107eabd8e594441509781fa77cfeb
benshidanny11 <benshidanny11@gmail.com>
2022-07-20 16:46:43 +0200

    Implement associations

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require './decorate'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
@@ -10,6 +10,7 @@ class Person < Nameable
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def of_age?
@@ -24,5 +25,42 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, self, book)
+  end
+end
+
+
+class Person
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @corrector = Corrector.new
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+  end
+
+  def can_use_services?
+    @age > 18 || @parent_permission == true
+  end
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
+  end
+
+  private
+
+  def of_age?
+    @age > 18
   end
 end
--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
 require_relative './person'
 
 class Student < Person
-  def initialize(classroom)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super()
+    super(name, age, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  @classroom = classroom
+   if(!classroom.students.include?(self)){
+    classroom.students.push(self)
+   }
+ 
 end
