28f291d212508a18ec9bac830079bf3c8933dd8e
benshidanny11 <benshidanny11@gmail.com>
2022-07-20 22:25:20 +0200

    Implement review feedback

--- student.rb
+++ student.rb
@@ -10,7 +10,9 @@ class Student < Person
   def play_hooky
     "¯\(ツ)/¯"
   end
-
-  @classroom = classroom
-  classroom.students.push(self) if classroom.students.include?(self)
+  
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
 end
